1efcb68a3fdb08b5a16acac190b1176be9031505
Yshrael pimentel <Ysp.rael@gmail.com>
2021-04-02 20:23:44 -0300

    base-002
    var's nome, idade e sexo funcionando

--- main.js
+++ main.js
@@ -13,10 +13,26 @@ function verificar() {
 // VAR IDADE
 	var idade = window.document.getElementById('idade')
 	var idad = Number(idade.value)
+<<<<<<< Updated upstream
 // VAR NACIONALIDADE
 
 
+=======
+/* VAR NACIONALIDADE
+	var select = window.document.getElementById('nações')
+	var value = select.options[select.selectedIndex].value
+//SE TIVER DADOSS INCORRETOS..*/
+	if (nome == 0 || idade <= 0 || idade <122) {
+		alert('[ERR0] Verifique os dados e tente novamente!')
+	}else{
+// SE NÃO..
+>>>>>>> Stashed changes
 // RESSULTADO LATERAL	
 	res.innerHTML  = `Nome: ${nome}<br>`
 	res.innerHTML += `Gênero: ${sexo}<br>`
 	res.innerHTML += `Idade: ${idad} anos<br>`
+<<<<<<< Updated upstream
+=======
+}
+}
+>>>>>>> Stashed changes
