de1ed266bab0b755d00829013276e5a899238dd6
Yshrael pimentel <Ysp.rael@gmail.com>
2021-04-03 22:44:18 -0300

    de var nome a bairro
    de nome a bairro funcionando

--- main.js
+++ main.js
@@ -2,37 +2,47 @@ function verificar() {
 // VAR NOME
 	var txtn = window.document.getElementById('txtn')
 	var nome = txtn.value
+
 // VAR SEXO 
 	var sex = window.document.getElementsByName('radx')
-	var sexo = ''
+	var genero = ''
 	if (sex[0].checked) {
-		sexo = 'Homem'
+		genero = 'Homem'
 	} else {
-		sexo = 'mulher'
+		genero = 'mulher'
 	}
+
 // VAR IDADE
 	var idade = window.document.getElementById('idade')
 	var idad = Number(idade.value)
-<<<<<<< Updated upstream
+
 // VAR NACIONALIDADE
+ 
+	var select = window.document.getElementById('paises')
+	var vpaís = select.options[select.selectedIndex].text
+
+// VAR CIDADE
+	var select = window.document.getElementById('cidade')
+	var vcidad = select.options[select.selectedIndex].text
+
+// VAR BAIRRO
+	var select = window.document.getElementById('bairro')
+	var vbairr = select.options[select.selectedIndex].text
 
 
-=======
-/* VAR NACIONALIDADE
-	var select = window.document.getElementById('nações')
-	var value = select.options[select.selectedIndex].value
-//SE TIVER DADOSS INCORRETOS..*/
+// SE TIVER DADOSS INCORRETOS..
 	if (nome == 0 || idade <= 0 || idade <122) {
 		alert('[ERR0] Verifique os dados e tente novamente!')
 	}else{
+
 // SE NÃO..
->>>>>>> Stashed changes
+
 // RESSULTADO LATERAL	
 	res.innerHTML  = `Nome: ${nome}<br>`
-	res.innerHTML += `Gênero: ${sexo}<br>`
+	res.innerHTML += `Gênero: ${genero}<br>`
 	res.innerHTML += `Idade: ${idad} anos<br>`
-<<<<<<< Updated upstream
-=======
+	res.innerHTML += `País: ${vpaís} <br>`
+	res.innerHTML += `Cidade: ${vcidad}<br>`
+	res.innerHTML += `bairro: ${vbairr}<br>`
 }
 }
->>>>>>> Stashed changes
